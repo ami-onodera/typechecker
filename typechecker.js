3835c8b59d61446d2b8845842da5cbb46e92010e
Ami Onodera <onodera.camila@gmail.com>
2020-11-11 15:53:57 +0100

    finished

--- typechecker.js
+++ typechecker.js
@@ -1,14 +1,22 @@
-const sentenceTag = document.querySelector('input[type="text"]');
+const sentenceTag = document.querySelector('input[type="text"]')
 
-const typesizeTag = document.querySelector('input[name="typesize"]');
-const typesizeOutput = document.querySelector('span.typesize-output');
+const typesizeTag = document.querySelector('input[name="typesize"]')
+const typesizeOutput = document.querySelector('span.typesize-output')
 
 const lineheightTag = document.querySelector('input[name="lineheight"]')
 const lineheightOutput = document.querySelector('span.lineheight-output')
 
+const fontweightTag = document.querySelector('input[name="fontweight"]')
+const fontweightOutput = document.querySelector('span.fontweight-output')
+
+
 const italicTag = document.querySelector('input[name="italic"]')
 
-const outputTag = document.querySelector("textarea.output");
+const typefaceTag = document.querySelector('select[name="typeface"]')
+
+const colorTags = document.querySelectorAll('div.colors div')
+
+const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.value;
 
 // when type the sentence, update the output. If there is no value, use original text
@@ -20,6 +28,7 @@ sentenceTag.addEventListener("keyup", function () {
         outputTag.value = originalText;
     }
 })
+
 
 // when I type in the output, update the sentence
 
@@ -41,6 +50,14 @@ lineheightTag.addEventListener("input", function() {
     lineheightOutput.innerHTML = this.value;
 })
 
+// font weight
+
+fontweightTag.addEventListener("input", function() {
+    outputTag.style.fontWeight = this.value;
+    fontweightOutput.innerHTML = this.value;
+})
+
+
 // when box is checked make italic
 
 italicTag.addEventListener("change", function() {
@@ -49,5 +66,26 @@ italicTag.addEventListener("change", function() {
     } else {
         outputTag.style.fontStyle = "normal"
     }
+})
+
+// change the font
+
+typefaceTag.addEventListener("input", function() {
+    outputTag.style.fontFamily = this.value;
+})
+
+// change background and text color, and make this tag as "selected"
+
+colorTags.forEach((tag) => {
+    tag.addEventListener("click", function() {
+        outputTag.style.backgroundColor = this.style.backgroundColor
+        outputTag.style.color = this.style.color
+
+        colorTags.forEach(tag => {
+            tag.classList.remove("selected")
+        })
+
+        this.classList.add("selected")
+    })
 })
 
